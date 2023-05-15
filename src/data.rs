29920ab59cfef8590ce2245b960c10febeef5b57
Ryota Sakamoto <skmt@amazon.com>
2023-05-15 23:55:22 +0900

    refactor: move key from app.rs to key.rs
    Key is used by some functions so it should be divided from app.rs

--- src/data.rs
+++ src/data.rs
@@ -34,6 +34,7 @@ use tabwriter::TabWriter;
 // use bytes::Bytes;
 
 use super::app;
+use super::key;
 
 /* =================================================
 struct / enum / const
@@ -897,7 +898,7 @@ fn generate_query_expressions(
     let expression: String = String::from("#DYNEIN_PKNAME = :DYNEIN_PKVAL");
     let mut names = HashMap::<String, String>::new();
     let mut vals = HashMap::<String, AttributeValue>::new();
-    let mut sort_key_of_target_table_or_index: Option<app::Key> = None;
+    let mut sort_key_of_target_table_or_index: Option<key::Key> = None;
 
     match index {
         None =>
@@ -980,7 +981,7 @@ fn generate_query_expressions(
 /// Using existing key condition expr (e.g. "myId <= :idVal") and supplementary mappings (expression_attribute_names, expression_attribute_values),
 /// this method returns GeneratedQueryParams struct. Note that it's called only when sort key expression (ske) exists.
 fn append_sort_key_expression(
-    sort_key: Option<app::Key>,
+    sort_key: Option<key::Key>,
     partition_key_expression: &str,
     sort_key_expression: &str,
     mut names: HashMap<String, String>,
